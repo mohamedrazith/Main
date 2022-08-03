d772e80077e018f464d68cdaa8293bbd513fbeb7
mohamedrazith <mohamedrazith2000@gmail.com>
2022-08-03 14:17:59 +0530

    init

---
+++ src/New.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class New {
+
+	public static void main(String[] args) {
+		Stack<String> games = new Stack<String>();
+
+		games.add("Call of Duty");
+		games.add("Guitar Hero");
+		games.add("Monkey Ball");
+
+		System.out.println(games);
+
+		System.out.println(games.pop());
+		System.out.println(games.pop());
+		System.out.println(games.peek());
+
+		System.out.println(games);
+
+		Stack<Character> tower = new Stack<Character>();
+		tower.add('R');
+		tower.add('B');
+		tower.add('Y');
+		System.out.println(tower.peek());
+		System.out.println(tower.pop());
+		System.out.println(tower.contains('P'));
+		System.out.println(tower.empty());
+		System.out.println(tower.get(1));
+		System.out.println(tower.size());
+
+	}
+
+}
